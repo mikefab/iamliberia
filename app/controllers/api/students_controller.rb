4f51b6e66add2fbaa51429a8fcbc6438a53bd85c
mikefab <mikefabrikant@gmail.com>
2015-06-22 12:15:03 +0000

    Sort users by last activity date

--- app/controllers/api/students_controller.rb
+++ app/controllers/api/students_controller.rb
@@ -3,17 +3,16 @@ class Api::StudentsController < ApplicationController
 
   def index
     students = []
-    User.all.each do |u|
+    User.all.sort_by{|u| u.last_activity_date}.reverse.each do |u|
       
       students << {
-        username: u.username,
-        name:     u.name,
-        profile_image: ActionController::Base.helpers.asset_path("assets/#{u.username}.jpg"),
-        langs:    u.last_lang_lesson[:langs],
-        calendar: u.last_lang_lesson[:calendar],
-        achievement_dates: u.last_lang_lesson[:achievement_dates],
-        khan_exercises: u.last_khan_exercise
-
+        username:           u.username,
+        name:               u.name,
+        langs:              u.last_lang_lesson[:langs],
+        calendar:           u.last_lang_lesson[:calendar],
+        achievement_dates:  u.last_lang_lesson[:achievement_dates],
+        khan_exercises:     u.last_khan_exercise,
+        profile_image:      ActionController::Base.helpers.asset_path("assets/#{u.username}.jpg")
       }
     end
     render json: students
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  devise :omniauthable, :omniauth_providers => [:google_oauth2]
+  devise :omniauthable, :omniauth_providers => [:google_oauth2, :facebook, :twitter]
 
   ## Database authenticatable
   field :email,              type: String, default: ""
@@ -27,16 +27,30 @@ class User
   field :current_sign_in_ip, type: String
   field :last_sign_in_ip,    type: String
   has_many :lessons, :foreign_key => 'username'
+
+  def last_activity_date
+    l = self.last_lesson
+    l.nil? ? Date.new(1900,1,1) : Date.parse(self.last_lesson.content.last['date'])
+    
+  end
+
   # Use email handle a username to access other services
   def username
     self.email.split('@')[0]
   end
 
-  def last_khan_exercise
-    lesson = Lesson.where(
+  def last_lesson
+    l = Lesson.where(
       username: self.username,
       source:   'khan'
-      ).last
+      )
+
+    l.nil? ? nil : l.last
+  end
+
+  def last_khan_exercise
+    
+    lesson = self.last_lesson
 
     exercises = lesson.content.reverse.map{
         |e| {
@@ -63,9 +77,7 @@ class User
       current_lng  = lng[0]
       # Hash of date and lesson name
 
-
       achievements = Lesson.achievements(lng[1], lesson['calendar']).sort_by{|o| o[:date]}.reverse
-
       current_hash = {fluency_score: lng[1]['fluency_score'], streak: lng[1]['streak']}
     end
 
@@ -87,16 +99,27 @@ class User
     {achievement_dates: achievements, langs: langs, calendar: lesson['calendar']}
   end
 
+  def self.from_omniauth(auth)
+    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+      user.provider = auth.provider 
+      user.uid = auth.uid
+      user.name = auth.info.name
+      user.oauth_token = auth.credentials.oauth_token 
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.save
+     end
+  end 
+
   def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
       data = access_token.info
       user = User.where(:email => data["email"]).first
 
       # Uncomment the section below if you want users to be created if they don't exist
       unless user
-          user = User.create(name: data["name"],
-             email: data["email"],
-             password: Devise.friendly_token[0,20]
-          )
+        user = User.create(name: data['name'],
+           email: data['email'],
+           password: Devise.friendly_token[0,20]
+        )
       end
       user
   end
