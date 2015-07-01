956229f5d32d6c225d09237f2cfedac485741909
mikefab <mikefabrikant@gmail.com>
2015-07-01 12:43:25 +0000

    Cache students for a day

--- app/controllers/api/students_controller.rb
+++ app/controllers/api/students_controller.rb
@@ -3,18 +3,23 @@ class Api::StudentsController < ApplicationController
 
   def index
     students = []
-    User.all.sort_by{|u| u.last_activity_date}.reverse.each do |u|
-      
-      students << {
-        username:           u.username,
-        name:               u.name,
-        langs:              u.last_lang_lesson[:langs],
-        calendar:           u.last_lang_lesson[:calendar],
-        achievement_dates:  u.last_lang_lesson[:achievement_dates],
-        khan_exercises:     u.last_khan_exercise,
-        codecademy_badges:  u.codecademy_badges,
-        profile_image:      ActionController::Base.helpers.asset_path("assets/#{u.username}.jpg")
-      }
+    if Rails.cache.exist?('students')
+      students = Rails.cache.read('students')
+    else
+      User.all.sort_by{|u| u.last_activity_date}.reverse.each do |u|
+        
+        students << {
+          username:           u.username,
+          name:               u.name,
+          langs:              u.last_lang_lesson[:langs],
+          calendar:           u.last_lang_lesson[:calendar],
+          achievement_dates:  u.last_lang_lesson[:achievement_dates],
+          khan_exercises:     u.last_khan_exercise,
+          codecademy_badges:  u.codecademy_badges,
+          profile_image:      ActionController::Base.helpers.asset_path("assets/#{u.username}.jpg")
+        }
+      end
+      Rails.cache.fetch('students', :expires_in => 1.day){students}
     end
     render json: students
   end
